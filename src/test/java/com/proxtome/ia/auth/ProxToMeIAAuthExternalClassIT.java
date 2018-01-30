d45607da8631a095db245421e219d897f01ce0e7
Walter Danilo Galante <walterdangalante@gmail.com>
2018-01-30 18:50:40 +0100

    Fixed failing test

--- src/test/java/com/proxtome/ia/auth/ProxToMeIAAuthExternalClassIT.java
+++ src/test/java/com/proxtome/ia/auth/ProxToMeIAAuthExternalClassIT.java
@@ -24,62 +24,30 @@ public class ProxToMeIAAuthExternalClassIT {
 
     @BeforeSuite
     public void getAuthToken() throws Exception {
-//        Map<String, String> nameCallbackInput = new HashMap<String, String>();
-//        nameCallbackInput.put("name", "IDToken1");
-//        nameCallbackInput.put("value", "demo");
-//        Map<String, Object> nameCallback = new HashMap<String, Object>();
-//        nameCallback.put("type", "NameCallback");
-//        List<Object> nameCallbackInputs = new ArrayList<Object>();
-//        nameCallbackInputs.add(nameCallbackInput);
-//        nameCallback.put("input", nameCallbackInputs);
-//
-//        Map<String, String> passwordCallbackInput = new HashMap<String, String>();
-//        passwordCallbackInput.put("name", "IDToken2");
-//        passwordCallbackInput.put("value", "changeit");
-//        Map<String, Object> passwordCallback = new HashMap<String, Object>();
-//        passwordCallback.put("type", "PasswordCallback");
-//        List<Object> passwordCallbackInputs = new ArrayList<Object>();
-//        passwordCallbackInputs.add(passwordCallbackInput);
-//        passwordCallback.put("input", passwordCallbackInputs);
-//
-//        Map<String, Object> payload = new HashMap<String, Object>();
-//        List<Object> callbacks = new ArrayList<Object>();
-//        callbacks.add(nameCallback);
-//        callbacks.add(passwordCallback);
-//        payload.put("callbacks", callbacks);
-//        String jsonPayload = new ObjectMapper().writeValueAsString(payload);
-//        String jsonPayload = "{\"callbacks\": " +
-//                "[{\"type\": \"NameCallback\", " +
-//                "\"input\": [{\"name\": \"IDToken1\", \"value\": \"demo\"]}, " +
-//                "{\"type\": \"PasswordCallback\", " +
-//                "\"input\": [{\"name\": \"IDToken2\", \"value\":\"changeit\"]}]}";
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost request = new HttpPost(AM_AUTHENTICATE_ENDPOINT);
         request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         request.setHeader("X-OpenAM-Username", "demo");
         request.setHeader("X-OpenAM-Password", "changeit");
-//        request.setEntity(new StringEntity(jsonPayload));
         HttpResponse response = client.execute(request);
         int statusCode = response.getStatusLine().getStatusCode();
         String jsonResponse = EntityUtils.toString(response.getEntity());
         System.out.println(jsonResponse);
         Assert.assertEquals(statusCode, 200);
         this.userToken = new ObjectMapper().reader().readTree(jsonResponse).get("tokenId").asText();
-        System.out.println("Token = " + this.userToken);
     }
 
     @Test
     public void testWorking() throws Exception {
-        System.out.print("Token = " + this.userToken);
         String jsonPayload = "{\"deviceId\": \"" + VALID_DEVICEID + "\", \"callbacks\": " +
                 "[{\"type\": \"NameCallback\", " +
-                "\"input\": [{\"name\": \"IDToken1\", \"value\": \"" + VALID_USERID + "\"]}, " +
+                "\"input\": [{\"name\": \"IDToken1\", \"value\": \"" + VALID_USERID + "\"}]}, " +
                 "{\"type\": \"NameCallback\", " +
-                "\"input\": [{\"name\": \"IDToken2\", \"value\": \"" + VALID_DEVICEID + "\"]}, " +
+                "\"input\": [{\"name\": \"IDToken2\", \"value\": \"" + VALID_DEVICEID + "\"}]}, " +
                 "{\"type\": \"PasswordCallback\", " +
-                "\"input\": [{\"name\": \"IDToken3\", \"value\": \"" + VALID_CHALLENGE + "\"]}, " +
+                "\"input\": [{\"name\": \"IDToken3\", \"value\": \"" + VALID_CHALLENGE + "\"}]}, " +
                 "{\"type\": \"PasswordCallback\", " +
-                "\"input\": [{\"name\": \"IDToken4\", \"value\":\"" + VALID_RESPONSE + "\"]}]}";
+                "\"input\": [{\"name\": \"IDToken4\", \"value\":\"" + VALID_RESPONSE + "\"}]}]}";
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost request = new HttpPost(AM_AUTHENTICATE_ENDPOINT +
                 "?authIndexType=module" +
@@ -90,7 +58,10 @@ public class ProxToMeIAAuthExternalClassIT {
         request.setEntity(new StringEntity(jsonPayload));
         HttpResponse response = client.execute(request);
         int statusCode = response.getStatusLine().getStatusCode();
-        System.out.println(EntityUtils.toString(response.getEntity()));
+        String jsonResponse = EntityUtils.toString(response.getEntity());
+        System.out.println(jsonResponse);
         Assert.assertEquals(statusCode, 200);
+        String updatedToken = new ObjectMapper().reader().readTree(jsonResponse).get("tokenId").asText();
+        Assert.assertNotNull(updatedToken);
     }
 }
